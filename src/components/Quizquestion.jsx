0994e7439fa67b6ed2709c60ea5c171904f346f7
Nishant6571 <nishantbhargava.bhargava@gmail.com>
2024-03-11 18:28:04 +0530

    clean css

--- src/components/Quizquestion.jsx
+++ src/components/Quizquestion.jsx
@@ -21,22 +21,33 @@ const QuizQuestion = ({
   };
 
   return (
-    <VStack align="flex-start">
-      <Text fontWeight="bold" mb={4}>
+    <VStack
+      align="flex-start"
+      spacing={4}
+      p={4}
+      boxShadow="md"
+      rounded="md"
+      bg="gray.100"
+    >
+      <Text fontWeight="bold" fontSize="lg">
         {questionText}
       </Text>
       {options.map((option, index) => (
         <Button
           key={index}
           variant="outline"
-          mb={2}
+          colorScheme="teal"
           onClick={() => handleOptionClick(option)}
         >
           {option}
         </Button>
       ))}
-      <Button onClick={onPreviousQuestion}>Previous</Button>
-      <Button onClick={onNextQuestion}>Next</Button>
+      <Button colorScheme="teal" onClick={onPreviousQuestion}>
+        Previous
+      </Button>
+      <Button colorScheme="teal" onClick={onNextQuestion}>
+        Next
+      </Button>
     </VStack>
   );
 };
